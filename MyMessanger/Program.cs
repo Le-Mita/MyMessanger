9d32defcec35ecf1f1f45b28874ed97f69a3225a
Lena_Mit <bezlemit@gmail.com>
2021-07-16 19:01:47 +1000

    save as json

--- MyMessanger/Program.cs
+++ MyMessanger/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace MyMessanger
 {
@@ -6,9 +7,16 @@ namespace MyMessanger
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine("Начало проекта!");
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("RusAl", "Hi!", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+            //{ "UserName":"RusAl","MessageText":"Hi!","TimeStamp":"2021-07-16T08:59:18.1836244Z"}
+            //RusAl < 7 / 16 / 2021 8:59:18 AM >: Hi!
+
+            //Console.WriteLine("Начало проекта!");
+            //Console.WriteLine(msg.ToString());
         }
     }
 }
