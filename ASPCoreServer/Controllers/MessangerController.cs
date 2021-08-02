b3aac0e2db8e373bebf9df9497d03d350819afed
Lena_Mit <bezlemit@gmail.com>
2021-08-02 20:31:41 +1000

    dobavila server

---
+++ ASPCoreServer/Controllers/MessangerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPCoreServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessangerController : ControllerBase
+    {
+        
+
+        // GET api/<MessangerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "hochu v VDV " + id.ToString();
+        }
+
+        // POST api/<MessangerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+    }
+}
